6780317af0a5ee97581c0bbb1fbf546612673268
fazzadwir <fazzariandy@gmail.com>
2024-07-17 13:34:15 +0700

    CRUD Role

--- src/views/product/EditProduct.vue
+++ src/views/product/EditProduct.vue
@@ -1,186 +1,220 @@
 <template>
-    <div class="container">
-        <div class="edit_container">
-            <div class="left">
-                <div class="img_container">
-                    <img :src="this.images" alt="Gambar Produk">
-                </div>
-            </div>
-            <div class="right">
-                    <div class="title">
-                        <h1 class="text-center white">Edit Produk</h1>
-                    <div class="body">
-                        <form>
-                            <div class="input">
-                                <span>Nama Produk</span>
-                                <b-form-input v-model="title" class="in"></b-form-input>
-                            </div>
-                            <div class="input">
-                                <span>Category</span>
-                                <b-form-select v-model="category" class="in mb-3" disabled>
-                                    <b-form-select-option v-for="(item, index) in categoryProductList" :key="index" :value="item.id">>{{ item.name }}</b-form-select-option>
-                                </b-form-select>
-                            </div>
-                            <div class="input">
-                                <span>Description</span>
-                                <b-form-textarea v-model="description" class="in"></b-form-textarea>
-                            </div>
-                            <div class="input">
-                                <span>Harga</span>
-                                <b-form-input type="number" v-model="price" class="in"></b-form-input>
-                            </div>
-                            <div class="input">
-                                <span>Ubah Gambar</span>
-                                <b-form-file @change="listenFile" class="in" accept="image/*"></b-form-file>
-                            </div>
-
-                            <div class="btn_group">
-                                <button class="btn_primary btn-wide" @click="editData">Ubah Produk</button>
-                                <button class="btn_delete btn-wide" @click="deleteItem">Hapus Produk</button>
-                                <button class="btn_secondary btn-wide" @click="routeToPageHome">Kembali</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="edit_container">
+      <div class="left">
+        <div class="img_container">
+          <img :src="this.images" alt="Gambar Produk" />
         </div>
+      </div>
+      <div class="right">
+        <div class="title">
+          <h1 class="text-center white">Edit Produk</h1>
+        </div>
+        <div class="body">
+          <form>
+            <div class="input">
+              <span>Nama Produk</span>
+              <b-form-input v-model="title" class="in"></b-form-input>
+            </div>
+            <div class="input">
+              <span>Category</span>
+              <b-form-select v-model="category" class="in mb-3" disabled>
+                <b-form-select-option
+                  v-for="(item, index) in categoryProductList"
+                  :key="index"
+                  :value="item.id"
+                  >{{ item.name }}</b-form-select-option
+                >
+              </b-form-select>
+            </div>
+            <div class="input">
+              <span>Description</span>
+              <b-form-textarea
+                v-model="description"
+                class="in"
+              ></b-form-textarea>
+            </div>
+            <div class="input">
+              <span>Harga</span>
+              <b-form-input
+                type="number"
+                v-model="price"
+                class="in"
+              ></b-form-input>
+            </div>
+            <div class="input">
+              <span>Ubah Gambar</span>
+              <b-form-file
+                @change="listenFile"
+                class="in"
+                accept="image/*"
+              ></b-form-file>
+            </div>
+
+            <div class="btn_group">
+              <button
+                v-if="userRole === 'admin'"
+                class="btn_primary btn-wide"
+                @click="editData"
+              >
+                Ubah Produk
+              </button>
+              <button
+                v-if="userRole === 'admin'"
+                class="btn_delete btn-wide"
+                @click="deleteItem"
+              >
+                Hapus Produk
+              </button>
+              <button class="btn_secondary btn-wide" @click="routeToPageHome">
+                Kembali
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-
-    
-
+  </div>
 </template>
 
 <script>
+import cookie from "js-cookie";
 
-export default{
-    name: 'Edit',
-    data() {
-        return {
-            mainId: "",
-            categoryId: "",
-            downloading: false,
-            categoryProductList: [],
-
-            title: "",
-            price: "",
-            description: "",
-            category: "",
-            images: "",
-
-            file: null
-        }
+export default {
+  name: "Edit",
+  data() {
+    return {
+      mainId: "",
+      categoryId: "",
+      downloading: false,
+      categoryProductList: [],
+      title: "",
+      price: "",
+      description: "",
+      category: "",
+      images: "",
+      file: null,
+      userRole: "",
+    };
+  },
+  methods: {
+    listenFile(event) {
+      this.file = event.target.files;
+      console.log("file", this.file[0]);
     },
-    methods: {
-        listenFile(event) {
-            this.file = event.target.files;
-            console.log('file', this.file[0]);
-        },
-        getMainId() {
-            console.log("id", this.$route.query.id)
+    getMainId() {
+      console.log("id", this.$route.query.id);
 
-            this.mainId = this.$route.query.id;
+      this.mainId = this.$route.query.id;
 
-            this.getProductById(this.mainId);
-        },
-        getProductById(id){
-            const timeout = 500;
+      this.getProductById(this.mainId);
+    },
+    getProductById(id) {
+      const timeout = 500;
 
-            this.$axios.get('products/' + id)
-            .then((res) => {
+      this.$axios.get("products/" + id).then((res) => {
+        this.downloading = true;
+        let productdata = res.data;
 
-                    this.downloading = true;
-                    let productdata = res.data;
-                        
-                        this.categoryId = productdata.category.id;
-                        this.title = productdata.title;
-                        this.price = productdata.price;
-                        this.description = productdata.description;
-                        this.category = this.categoryId;
-                        this.images = this.regex(productdata.images[0])
+        this.categoryId = productdata.category.id;
+        this.title = productdata.title;
+        this.price = productdata.price;
+        this.description = productdata.description;
+        this.category = this.categoryId;
+        this.images = this.regex(productdata.images[0]);
+      });
 
-            })
+      this.downloading = false;
+    },
+    routeToPageHome() {
+      this.$router.push("/product");
+    },
+    editData() {
+      let image = null;
 
-        this.downloading = false;
+      if (this.file !== null) {
+        let formData = new FormData();
+        formData.append("file", this.file[0]);
 
-        },
-        routeToPageHome(){
-            this.$router.push('/product');
-        },
-        editData(){
-            let image = null;
+        this.$axios
+          .post("files/upload", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            alert("File ditambah!");
+            console.log("response", response.data.location);
+            image = response.data.location;
 
-            if(this.file !== null){
-                let formData = new FormData();
-                formData.append("file", this.file[0]);
+            this.putData(image);
+          });
+      } else {
+        this.putData(false);
+      }
 
-                this.$axios.post('files/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-                }).then((response) =>{
-                    alert('FIle ditambah!')
-                    console.log('response', response.data.location)
-                    image = response.data.location;
-
-                    this.putData(image)
-                })
-            }else{
-                this.putData(false)
-            }
-
-            console.log('image', image)
-
-                       
-        },
-        getListDataCategory(){
-            this.$axios.get('categories')
-            .then(res => {
-                this.categoryProductList = res.data;
-            })
-        },
-        regex(text) {
-            const regex_txt = /(https?:\/\/[^\s"]+)/g;
-            const cek = text.match(regex_txt);
-            if (cek) {
-                return cek[0];
-            }
-            return "";
-        },
-        putData(image){
-            this.$axios.put('products/' + this.mainId, {
-                title: this.title,
-                price: this.price,
-                description: this.description,
-                categoryId: this.category,
-                images: image?[image]:[this.images]
-                }).then(() =>{
-                        alert('Data diubah!')
-                        this.$router.push({path: '/product'})
-                    }) 
-        },
-        deleteItem(){
-            this.$Swal.fire({
-                title: "Apakah Anda yakin?",
-                showDenyButton: true,
-                confirmButtonText: "Tidak",
-                denyButtonText: `Ya`
-            }).then((result) => {
-                if (result.isDenied) {
-                    this.$axios.delete('products/' + this.mainId).then(()=>{
-                        this.$Swal.fire({
-                            title: "Produk Dihapus!",
-                            icon: "success"
-                        });
-                    this.$router.push({path: '/product'})
-                    })
-                }
+      console.log("image", image);
+    },
+    getListDataCategory() {
+      this.$axios.get("categories").then((res) => {
+        this.categoryProductList = res.data;
+      });
+    },
+    regex(text) {
+      const regex_txt = /(https?:\/\/[^\s"]+)/g;
+      const cek = text.match(regex_txt);
+      if (cek) {
+        return cek[0];
+      }
+      return "";
+    },
+    putData(image) {
+      this.$axios
+        .put("products/" + this.mainId, {
+          title: this.title,
+          price: this.price,
+          description: this.description,
+          categoryId: this.category,
+          images: image ? [image] : [this.images],
+        })
+        .then(() => {
+          alert("Data diubah!");
+          this.$router.push({ path: "/product" });
+        });
+    },
+    deleteItem() {
+      this.$Swal
+        .fire({
+          title: "Apakah Anda yakin?",
+          showDenyButton: true,
+          confirmButtonText: "Tidak",
+          denyButtonText: `Ya`,
+        })
+        .then((result) => {
+          if (result.isDenied) {
+            this.$axios.delete("products/" + this.mainId).then(() => {
+              this.$Swal.fire({
+                title: "Produk Dihapus!",
+                icon: "success",
+              });
+              this.$router.push({ path: "/product" });
             });
-        },
+          }
+        });
     },
-    mounted(){
-        this.getMainId();
-        this.getListDataCategory();
-    }
-}
-
+    fetchUserRole() {
+      const userdata = cookie.getJSON("userdata");
+      if (userdata && userdata.role) {
+        this.userRole = userdata.role;
+      } else {
+        console.error("User role is not available in the cookie");
+      }
+    },
+  },
+  mounted() {
+    this.getMainId();
+    this.getListDataCategory();
+    this.fetchUserRole();
+  },
+};
 </script>
--- src/views/product/ListProduct.vue
+++ src/views/product/ListProduct.vue
@@ -1,149 +1,187 @@
 <template>
-    <div class="container">
-            <button class="btn_primary btn-wide" @click="routeAddProduct">Tambah Produk</button>
-            <div class="bar_container">
-                <div class="search">
-                    <input type="text" v-model="search_value">
-                    <button class="btn_search" @click="search">Cari</button>
-                </div>
-                <div class="filter">
-                    <b-dropdown text="Urutkan">
-                        <b-dropdown-item value="1" class="primary_hover" @click="listenToSort(1)">Semua</b-dropdown-item>
-                        <b-dropdown-item  value="2" class="primary_hover" @click="listenToSort(2)">Termahal</b-dropdown-item>
-                        <b-dropdown-item  value="3" class="primary_hover" @click="listenToSort(3)">Termurah</b-dropdown-item>
-                    </b-dropdown>
-                </div>
-            </div>
-            <div class="product_container">
-                <div class="boxs" v-for="(item, index) in dataProductList" :key="index" @click="routeToEdit(item.id)">
-                    <div class="detail"></div>
-                    <div class="up">
-                        <div class="image">
-                            <img :src="item.image1" alt="product image">
-                        </div>
-                        <div class="wide">
-                            <h3>{{item.title}}</h3>
-                        </div>
-                    </div>
-                    <div class="down">
-                        <div class="others"><span class="price">{{ item.price }}</span></div>
-                        <div class="others">{{ item.category}}</div>
-                    </div>
-                </div>
-            </div>
+  <div class="container">
+    <button
+      v-if="userRole === 'admin'"
+      class="btn_primary btn-wide"
+      @click="routeAddProduct"
+    >
+      Tambah Produk
+    </button>
+    <div class="bar_container">
+      <div class="search">
+        <input type="text" v-model="search_value" />
+        <button class="btn_search" @click="search">Cari</button>
+      </div>
+      <div class="filter">
+        <b-dropdown text="Urutkan">
+          <b-dropdown-item
+            value="1"
+            class="primary_hover"
+            @click="listenToSort(1)"
+            >Semua</b-dropdown-item
+          >
+          <b-dropdown-item
+            value="2"
+            class="primary_hover"
+            @click="listenToSort(2)"
+            >Termahal</b-dropdown-item
+          >
+          <b-dropdown-item
+            value="3"
+            class="primary_hover"
+            @click="listenToSort(3)"
+            >Termurah</b-dropdown-item
+          >
+        </b-dropdown>
+      </div>
     </div>
-
+    <div class="product_container">
+      <div
+        class="boxs"
+        v-for="(item, index) in dataProductList"
+        :key="index"
+        @click="routeToEdit(item.id)"
+      >
+        <div class="detail"></div>
+        <div class="up">
+          <div class="image">
+            <img :src="item.image1" alt="product image" />
+          </div>
+          <div class="wide">
+            <h3>{{ item.title }}</h3>
+          </div>
+        </div>
+        <div class="down">
+          <div class="others">
+            <span class="price">{{ item.price }}</span>
+          </div>
+          <div class="others">{{ item.category }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import cookie from "js-cookie";
 
-export default{
-    name: 'addProduct',
-    data() {
-        return {
-            downloading: false,
-            dataProductList: [],
-            input: "",
-            search_value: "",
-            temp: [],
-            sort: ""
-        }
+export default {
+  name: "addProduct",
+  data() {
+    return {
+      downloading: false,
+      dataProductList: [],
+      input: "",
+      search_value: "",
+      temp: [],
+      sort: "",
+      userRole: "",
+    };
+  },
+  methods: {
+    getListDataProduct() {
+      const timeout = 500;
+
+      this.$axios.get("products").then((res) => {
+        setTimeout(() => {
+          this.downloading = true;
+          let productdata = res.data;
+
+          productdata.forEach((item) => {
+            this.dataProductList.push({
+              id: item.id ? item.id : -1,
+              title: item.title ? this.limitText(item.title, 20) : "no title",
+              price: item.price ? item.price : 0,
+              description: item.description
+                ? item.description
+                : "no description",
+              category: item.category.name
+                ? this.limitText(item.category.name, 15)
+                : "no category",
+              image: item.category.image ? item.category.image : "no image",
+              categoryId: item.category.id ? item.category.id : -1,
+              image1: item.images[0] ? this.regex(item.images[0]) : "no image",
+              // 'image2': item.images[1]?item.images[1]:'no image',
+              // 'image3': item.images[2]?item.images[2]:'no image',
+            });
+          });
+        }, timeout);
+      });
+
+      this.downloading = false;
+      console.log("dataProductList", this.dataProductList);
     },
-    methods: {
-        getListDataProduct(){
-            const timeout = 500;
-
-            this.$axios.get('products')
-            .then(res => {
-
-                setTimeout(() => {
-
-                    this.downloading = true;
-                    let productdata = res.data;
-                    
-
-                    productdata.forEach(item =>{
-                        this.dataProductList.push({
-                            'id': item.id?item.id:-1,
-                            'title': item.title?this.limitText(item.title, 20):'no title',
-                            'price': item.price?item.price:0,
-                            'description': item.description?item.description:'no description',
-                            'category': item.category.name?this.limitText(item.category.name, 15):'no category',
-                            'image': item.category.image?item.category.image:'no image',
-                            'categoryId': item.category.id?item.category.id:-1,
-                            'image1': item.images[0]?this.regex(item.images[0]):'no image',
-                            // 'image2': item.images[1]?item.images[1]:'no image',
-                            // 'image3': item.images[2]?item.images[2]:'no image',
-                        })
-                    })
-                     
-                }, timeout);
-            })
-            
-            this.downloading = false;
-            console.log("dataProductList", this.dataProductList)
-
-
-        },
-        routeToEdit(id){
-            this.$router.push('editProduct/?id=' + id);
-        },
-        routeAddProduct(){
-            this.$router.push('addProduct');
-        },
-        regex(text) {
-            const regex_txt = /(https?:\/\/[^\s"]+)/g;
-            const cek = text.match(regex_txt);
-            if (cek) {
-                return cek[0];
-            }
-            return "";
-        },
-        deleteItem(id){
-            if(confirm("yakin?")){
-                this.$axios.delete('products/' + id).then(()=>{
-                    alert("Data Berhasil dihapus!")
-                    window.location.reload();
-                })
-            }
-        },
-        limitText(text, max){
-            if(text.length > 10){
-                const limitedText = text.slice(0, max)
-
-                return limitedText + "..."
-            }else{
-                return text
-            }
-        },
-        search(){
-            if(this.search_value){
-                let search = this.search_value.toLowerCase().replaceAll(" ", "")
-                this.dataProductList = [...this.temp].filter((product) =>
-                    product.title.toLowerCase().replaceAll(" ", "").includes(search)
-                );
-            }else{
-                this.dataProductList = this.temp;
-            }
-        },
-        listenToSort(data){
-            switch (data) {
-                case 1:
-                    this.dataProductList = [...this.temp]
-                    break;
-                case 2:
-                    this.dataProductList = [...this.temp].sort((a, b) => b.price - a.price)
-                    break;
-                default:
-                    this.dataProductList = [...this.temp].sort((a, b) => a.price - b.price)
-                    break;
-            }
-        }
-
+    routeToEdit(id) {
+      this.$router.push("editProduct/?id=" + id);
     },
-    mounted() {
-        this.getListDataProduct();
-        this.temp = this.dataProductList;
-    }
-}
+    routeAddProduct() {
+      this.$router.push("addProduct");
+    },
+    regex(text) {
+      const regex_txt = /(https?:\/\/[^\s"]+)/g;
+      const cek = text.match(regex_txt);
+      if (cek) {
+        return cek[0];
+      }
+      return "";
+    },
+    deleteItem(id) {
+      if (confirm("yakin?")) {
+        this.$axios.delete("products/" + id).then(() => {
+          alert("Data Berhasil dihapus!");
+          window.location.reload();
+        });
+      }
+    },
+    limitText(text, max) {
+      if (text.length > 10) {
+        const limitedText = text.slice(0, max);
+
+        return limitedText + "...";
+      } else {
+        return text;
+      }
+    },
+    search() {
+      if (this.search_value) {
+        let search = this.search_value.toLowerCase().replaceAll(" ", "");
+        this.dataProductList = [...this.temp].filter((product) =>
+          product.title.toLowerCase().replaceAll(" ", "").includes(search)
+        );
+      } else {
+        this.dataProductList = this.temp;
+      }
+    },
+    listenToSort(data) {
+      switch (data) {
+        case 1:
+          this.dataProductList = [...this.temp];
+          break;
+        case 2:
+          this.dataProductList = [...this.temp].sort(
+            (a, b) => b.price - a.price
+          );
+          break;
+        default:
+          this.dataProductList = [...this.temp].sort(
+            (a, b) => a.price - b.price
+          );
+          break;
+      }
+    },
+    fetchUserRole() {
+      const userdata = cookie.getJSON("userdata");
+      if (userdata && userdata.role) {
+        this.userRole = userdata.role;
+      } else {
+        console.error("User role is not available in the cookie");
+      }
+    },
+  },
+  mounted() {
+    this.getListDataProduct();
+    this.temp = this.dataProductList;
+    this.fetchUserRole();
+  },
+};
 </script>
